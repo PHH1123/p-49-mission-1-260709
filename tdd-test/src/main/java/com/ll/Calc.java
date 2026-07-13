ae418286a123753abb10ef857acae1ae2bd337f8
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-07-13 11:31:11 +0900

    Add: [Calc.java - minus(int num1, int num2)], [CalcTest.java - t5(), t6(), t7()]  Modify: Calc.java - run(String str)

--- tdd-test/src/main/java/com/ll/Calc.java
+++ tdd-test/src/main/java/com/ll/Calc.java
@@ -5,10 +5,24 @@ public class Calc {
     public static int run(String str) {
         String[] splitStr = str.split(" ");
 
-        return sum(Integer.parseInt(splitStr[0]), Integer.parseInt(splitStr[2]));
+        int num1 = Integer.parseInt(splitStr[0]);
+        int num2 = Integer.parseInt(splitStr[2]);
+        Integer rst = null;
+
+        if (splitStr[1].equals("+")) {
+            rst = sum(num1, num2);
+        } else if (splitStr[1].equals("-")) {
+            rst = minus(num1, num2);
+        }
+
+        return rst;
     }
 
     private static int sum(int num1, int num2) {
         return num1 + num2;
+    }
+
+    private static int minus(int num1, int num2) {
+        return num1 - num2;
     }
 }
