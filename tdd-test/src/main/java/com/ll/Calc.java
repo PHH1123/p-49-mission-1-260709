9f92b2135cdbfffbca1dfba5b53cdd57dfba355a
PHH1123 <gsjh2001@koreatech.ac.kr>
2026-07-13 11:40:51 +0900

    Add: CalcTest.java - t8(), t9(), t10(), t11()  Modify: Calc.java - run(String str)

--- tdd-test/src/main/java/com/ll/Calc.java
+++ tdd-test/src/main/java/com/ll/Calc.java
@@ -5,14 +5,18 @@ public class Calc {
     public static int run(String str) {
         String[] splitStr = str.split(" ");
 
-        int num1 = Integer.parseInt(splitStr[0]);
-        int num2 = Integer.parseInt(splitStr[2]);
         Integer rst = null;
 
-        if (splitStr[1].equals("+")) {
-            rst = sum(num1, num2);
-        } else if (splitStr[1].equals("-")) {
-            rst = minus(num1, num2);
+        for (int i=0; i < splitStr.length-2;i+=2) {
+            int num1 = Integer.parseInt(splitStr[i]);
+            int num2 = Integer.parseInt(splitStr[i+2]);
+
+            if (splitStr[i+1].equals("+")) {
+                rst = sum(num1, num2);
+            } else if (splitStr[i+1].equals("-")) {
+                rst = minus(num1, num2);
+            }
+            splitStr[i+2] = String.valueOf(rst);
         }
 
         return rst;
